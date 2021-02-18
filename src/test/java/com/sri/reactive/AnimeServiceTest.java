c3d230c2ccb5b86055e655d64f2ac44eaa6b0d09
sri <only4programming03@gmail.com>
2021-02-18 13:22:22 -0500

    added test for update anime

--- src/test/java/com/sri/reactive/AnimeServiceTest.java
+++ src/test/java/com/sri/reactive/AnimeServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.sri.reactive.domain.Anime;
+import com.sri.reactive.exception.AnimeNotfoundException;
 import com.sri.reactive.repository.AnimeRepository;
 import com.sri.reactive.service.AnimateService;
 import com.sri.reactive.util.AnimeCreator;
@@ -49,6 +50,8 @@ public class AnimeServiceTest {
 	public void SetUp() {
 		BDDMockito.when(animeRepository.findAll()).thenReturn(Flux.just(new Anime(1, "Sri")));
 		BDDMockito.when(animeRepository.findById(ArgumentMatchers.anyInt())).thenReturn(Mono.just(animeById));
+		BDDMockito.when(animeRepository.save(anime)).thenReturn(Mono.just(animeById));
+		BDDMockito.when(animeRepository.deleteById(ArgumentMatchers.anyInt())).thenReturn(Mono.empty());
 	}
 
 	@Test
@@ -59,11 +62,46 @@ public class AnimeServiceTest {
 	}
 
 	@Test
-	public void FindById_test() {
-	
-		StepVerifier.create(animeService.findAnimeById(1))
-		.expectSubscription()
-		.expectNext(animeById)
-		.verifyComplete();
+	public void FindById_Success_test() {
+
+		StepVerifier.create(animeService.findAnimeById(1)).expectSubscription().expectNext(animeById).verifyComplete();
+	}
+
+	@Test
+	public void FindById_Error_test() {
+
+		BDDMockito.when(animeRepository.findById(ArgumentMatchers.anyInt()))
+				.thenReturn(Mono.error(new AnimeNotfoundException("no anime of given id")));
+
+		StepVerifier.create(animeService.findAnimeById(1)).expectSubscription()
+				.expectErrorMessage("no anime of given id").verify();
+	}
+
+	@Test
+	public void saveAnime_test() {
+
+		StepVerifier.create(animeService.addAnimeUser(anime)).expectSubscription().expectNext(animeById)
+				.verifyComplete();
+	}
+
+	@Test
+	public void deleteAnime_test() {
+
+		StepVerifier.create(animeService.deleteAnimeById(1)).expectSubscription().expectComplete();
+	}
+
+	@Test
+	public void deleteAnime_Error_test() {
+		BDDMockito.when(animeRepository.deleteById(ArgumentMatchers.anyInt()))
+				.thenReturn(Mono.error(new AnimeNotfoundException("no anime found to delete")));
+
+		StepVerifier.create(animeService.deleteAnimeById(1)).expectSubscription()
+		.expectError(AnimeNotfoundException.class);
+	}
+	@Test
+	public void UpdateAnime_Success_test() {
+		BDDMockito.when(animeRepository.save(animeById)).thenReturn(Mono.empty());
+
+		StepVerifier.create(animeService.updateAnimeUser(animeById)).expectSubscription();
 	}
 }
